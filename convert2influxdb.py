d5807f2eb2b9a224ce540fc9d12c418fe6a5917c
Stefano Acerbetti <acerbetti@gmail.com>
2019-01-17 16:17:33 -0800

    Logging data using the pacific time zone

--- convert2influxdb.py
+++ convert2influxdb.py
@@ -1,25 +1,30 @@
-import datetime
-
-import sys
-from dateutil.parser import parse
 import csv
-epoch = datetime.datetime(1970,1,1)
+import sys
+import time
+
+from datetime import datetime
+from pytz import timezone
 
 reader = csv.reader(sys.stdin, delimiter=',', quotechar='"')
+
 for row in reader:
     # Skip headers
     if len(row) > 1 and 'usage' in row[0]:
         if 'gas' in row[0]:
             # Gas is daily
-            b = parse(row[1])
-            timestamp = int((b - epoch).total_seconds() * 1000000000)
+            datetime_obj_naive = datetime.strptime(row[1], "%Y-%m-%d")
+            datetime_obj_pacific = timezone('US/Pacific').localize(datetime_obj_naive)
+            timestamp = time.mktime(datetime_obj_pacific.timetuple())
+
             print "energy,energy_type=gas,value_type=usage value=%s %s" % (row[2], timestamp)
             if row[4][1:] != "":
                 print "energy,energy_type=gas,value_type=cost value=%s %s" % (row[4][1:], timestamp)
         else:
             # Electricity is hourly
-            b = parse("%s %s" % (row[1], row[2]))
-            timestamp = int((b - epoch).total_seconds() * 1000000000)
+            datetime_obj_naive = datetime.strptime(row[1] + " " + row[3], "%Y-%m-%d %H:%M")
+            datetime_obj_pacific = timezone('US/Pacific').localize(datetime_obj_naive)
+            timestamp = time.mktime(datetime_obj_pacific.timetuple())
+
             print "energy,energy_type=electric,value_type=usage value=%s %s" % (row[4], timestamp)
             if row[6][1:] != "":
                 print "energy,energy_type=electric,value_type=cost value=%s %s" % (row[6][1:], timestamp)
