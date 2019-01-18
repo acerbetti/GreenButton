4aa01bc6ad074e87946c155a0a19a6e950c5279f
Stefano Acerbetti <acerbetti@gmail.com>
2019-01-17 19:14:49 -0800

    Fix the timestamp format

--- convert2influxdb.py
+++ convert2influxdb.py
@@ -14,19 +14,19 @@ for row in reader:
             # Gas is daily
             datetime_obj_naive = datetime.strptime(row[1], "%Y-%m-%d")
             datetime_obj_pacific = timezone('US/Pacific').localize(datetime_obj_naive)
-            timestamp = time.mktime(datetime_obj_pacific.timetuple())
+            timestamp = time.mktime(datetime_obj_pacific.timetuple()) * 1000000000
 
-            print "energy,energy_type=gas,value_type=usage value=%s %s" % (row[2], timestamp)
+            print "energy,energy_type=gas,value_type=usage value=%s %i" % (row[2], timestamp)
             if row[4][1:] != "":
-                print "energy,energy_type=gas,value_type=cost value=%s %s" % (row[4][1:], timestamp)
+                print "energy,energy_type=gas,value_type=cost value=%s %i" % (row[4][1:], timestamp)
         else:
             # Electricity is hourly
             datetime_obj_naive = datetime.strptime(row[1] + " " + row[3], "%Y-%m-%d %H:%M")
             datetime_obj_pacific = timezone('US/Pacific').localize(datetime_obj_naive)
-            timestamp = time.mktime(datetime_obj_pacific.timetuple())
+            timestamp = time.mktime(datetime_obj_pacific.timetuple()) * 1000000000
 
-            print "energy,energy_type=electric,value_type=usage value=%s %s" % (row[4], timestamp)
+            print "energy,energy_type=electric,value_type=usage value=%s %i" % (row[4], timestamp)
             if row[6][1:] != "":
-                print "energy,energy_type=electric,value_type=cost value=%s %s" % (row[6][1:], timestamp)
+                print "energy,energy_type=electric,value_type=cost value=%s %i" % (row[6][1:], timestamp)
 
         # print ', '.join(row)
